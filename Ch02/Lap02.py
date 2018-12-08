b5a7dae6ac7269aece0550872e3affa90034a145
hhl5782 <45708154+hhl5782@users.noreply.github.com>
2018-12-08 17:05:39 +0900

    Ch02

---
+++ Ch02/Lap02.py
@@ -0,0 +1,7 @@
+studium = input("경기장은 어디입니까")
+winner = input("이긴팀은 어디입니까?")
+loser = input("진팀은 어디입니까?")
+vip = input("우수선수는 누구입니까?")
+score = input("스코어는 몇대몇입니까?")
+
+print("")
---
+++ Ch02/MyPython/변수.py
@@ -0,0 +1,13 @@
+import turtle
+t = turtle.Turtle()
+t.shape("turtle")
+a = 110
+b = 90
+t.forward(a)
+t.left(b)
+t.forward(a)
+t.left(b)
+t.forward(a)
+t.left(b)
+t.forward(a)
+t.left(b)
